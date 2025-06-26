4860329f581cca5ec52bbac642f01a952b2a0cbb
iizaghz <sukmahawaiza@gmail.com>
2025-06-26 15:54:50 +0700

    if the value in the data field of new node is less than that of parent

--- BinarySearchTree/BinaryTree.cpp
+++ BinarySearchTree/BinaryTree.cpp
@@ -70,6 +70,16 @@ public:
             // 5a : exit
             return;
         }
-       
+        // step 6 : if the value in the data field of new node is less than that of parent
+        if (x < parent->info)
+        {
+            // 6a : make the left child of parent point to the new Node
+            parent->leftchild = newNode;
+
+            // 6b : exit
+            return;
+        }
+
+        
     }
 };
